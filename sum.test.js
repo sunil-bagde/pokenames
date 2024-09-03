af3a80e581dbfc641d8a15d03b796a8b5fee8b55
Sunil Bagde <bagdesunil786@gmail.com>
2024-09-03 20:42:39 +0530

    feat: cicd

--- sum.test.js
+++ sum.test.js
@@ -2,7 +2,7 @@ test("adds 1 + 2 to equal 3", () => {
   expect(sum(1, 2)).toBe(3);
 });
 test("adds 5-2 to equal 3", () => {
-  expect(ssubum(5, 2)).toBe(3);
+  expect(sub(5, 2)).toBe(3);
 });
 function sum(a, b) {
   return a + b;
