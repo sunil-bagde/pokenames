7e3bcd34d2347fde1a5c02530a01aaf82c1e58f1
Sunil Bagde <bagdesunil786@gmail.com>
2024-09-03 20:41:47 +0530

    feat: cicd

--- src/index.mjs
+++ src/index.mjs
@@ -3,5 +3,6 @@ import pokeName from "./pokename.json" assert { type: "json" };
 import uniqueRandomArray from "unique-random-array";
 
 const randon = uniqueRandomArray(pokeName);
+const unique = uniqueRandomArray(pokeName);
 
-export { pokeName as all, randon };
+export { pokeName as all, randon, unique };
--- sum.test.js
+++ sum.test.js
@@ -1,6 +1,12 @@
 test("adds 1 + 2 to equal 3", () => {
   expect(sum(1, 2)).toBe(3);
 });
+test("adds 5-2 to equal 3", () => {
+  expect(ssubum(5, 2)).toBe(3);
+});
 function sum(a, b) {
   return a + b;
+}
+function sub(a, b) {
+  return a - b;
 }
