981dff8149d93856287a389f3886902c9c8c38f4
Sunil Bagde <bagdesunil786@gmail.com>
2024-09-04 17:31:09 +0530

    feat: cicd

--- src/index.mjs
+++ src/index.mjs
@@ -3,6 +3,6 @@ import pokeName from "./pokename.json"
 import uniqueRandomArray from "unique-random-array";
 
 const randon = uniqueRandomArray(pokeName);
-const unique = uniqueRandomArray(pokeName);
+const unique1 = uniqueRandomArray(pokeName);
 
-export { pokeName as all, randon, unique };
+export { pokeName as all, randon, unique1 };
